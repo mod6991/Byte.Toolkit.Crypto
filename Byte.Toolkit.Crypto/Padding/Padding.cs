9f8b21daa58fb9b4bc85bf5aa224eba850ca1665
Josue Clement <mod6991@gmail.com>
2021-12-10 21:46:03 +0100

    Add KDF, Padding and PubKey from DNTB 7.10

---
+++ Byte.Toolkit.Crypto/Padding/Padding.cs
@@ -0,0 +1,206 @@
+﻿using System.Security.Cryptography;
+
+namespace Byte.Toolkit.Crypto.Padding
+{
+    public class PaddingException : Exception
+    {
+        public PaddingException(string message) : base(message) { }
+    }
+
+    public enum PaddingStyle
+    {
+        None,
+        Pkcs7,
+        AnsiX923,
+        Iso7816,
+        Iso10126
+    }
+
+    public static class Padding
+    {
+        public static byte[] Pad(byte[] data, int blockSize, PaddingStyle paddingStyle)
+        {
+            switch (paddingStyle)
+            {
+                case PaddingStyle.None:
+                    return data;
+                case PaddingStyle.Pkcs7:
+                    return PadPkcs7(data, blockSize);
+                case PaddingStyle.AnsiX923:
+                    return PadAnsiX923(data, blockSize);
+                case PaddingStyle.Iso7816:
+                    return PadIso7816(data, blockSize);
+                case PaddingStyle.Iso10126:
+                    return PadIso10126(data, blockSize);
+                default:
+                    throw new InvalidOperationException("Unknown padding style");
+            }
+        }
+
+        public static byte[] Unpad(byte[] data, int blockSize, PaddingStyle paddingStyle)
+        {
+            switch (paddingStyle)
+            {
+                case PaddingStyle.None:
+                    return data;
+                case PaddingStyle.Pkcs7:
+                    return UnpadPkcs7(data, blockSize);
+                case PaddingStyle.AnsiX923:
+                    return UnpadAnsiX923(data, blockSize);
+                case PaddingStyle.Iso7816:
+                    return UnpadIso7816(data, blockSize);
+                case PaddingStyle.Iso10126:
+                    return UnpadIso10126(data, blockSize);
+                default:
+                    throw new InvalidOperationException("Unknown padding style");
+            }
+        }
+
+        public static byte[] PadPkcs7(byte[] data, int blockSize)
+        {
+            if (data == null)
+                throw new ArgumentNullException("data");
+
+            int paddingLength = blockSize - data.Length % blockSize;
+
+            byte[] paddedData = new byte[data.Length + paddingLength];
+            Array.Copy(data, 0, paddedData, 0, data.Length);
+            for (int i = data.Length; i < paddedData.Length; i++)
+                paddedData[i] = (byte)paddingLength;
+
+            return paddedData;
+        }
+
+        public static byte[] UnpadPkcs7(byte[] data, int blockSize)
+        {
+            if (data == null)
+                throw new ArgumentNullException("data");
+
+            if (data.Length % blockSize != 0 || data.Length == 0)
+                throw new PaddingException("Data is not padded");
+
+            byte paddingLength = data[data.Length - 1];
+
+            for (int i = data.Length - 2; i > data.Length - paddingLength - 1; i--)
+            {
+                if (data[i] != paddingLength)
+                    throw new PaddingException("Invalid Pkcs7 padding");
+            }
+
+            byte[] unpaddedData = new byte[data.Length - paddingLength];
+            Array.Copy(data, 0, unpaddedData, 0, data.Length - paddingLength);
+
+            return unpaddedData;
+        }
+
+        public static byte[] PadAnsiX923(byte[] data, int blockSize)
+        {
+            if (data == null)
+                throw new ArgumentNullException("data");
+
+            int paddingLength = blockSize - data.Length % blockSize;
+
+            byte[] paddedData = new byte[data.Length + paddingLength];
+            Array.Copy(data, 0, paddedData, 0, data.Length);
+            for (int i = data.Length; i < paddedData.Length - 1; i++)
+                paddedData[i] = 0;
+            paddedData[paddedData.Length - 1] = (byte)paddingLength;
+
+            return paddedData;
+        }
+
+        public static byte[] UnpadAnsiX923(byte[] data, int blockSize)
+        {
+            if (data == null)
+                throw new ArgumentNullException("data");
+
+            if (data.Length % blockSize != 0 || data.Length == 0)
+                throw new PaddingException("Data is not padded");
+
+            byte paddingLength = data[data.Length - 1];
+
+            for (int i = data.Length - 2; i > data.Length - paddingLength - 1; i--)
+            {
+                if (data[i] != 0)
+                    throw new PaddingException("Invalid AnsiX923 padding");
+            }
+
+            byte[] unpaddedData = new byte[data.Length - paddingLength];
+            Array.Copy(data, 0, unpaddedData, 0, data.Length - paddingLength);
+
+            return unpaddedData;
+        }
+
+        public static byte[] PadIso7816(byte[] data, int blockSize)
+        {
+            if (data == null)
+                throw new ArgumentNullException("data");
+
+            int paddingLength = blockSize - data.Length % blockSize;
+
+            byte[] paddedData = new byte[data.Length + paddingLength];
+            Array.Copy(data, 0, paddedData, 0, data.Length);
+
+            paddedData[data.Length] = 0x80;
+
+            for (int i = data.Length + 1; i < paddedData.Length; i++)
+                paddedData[i] = 0;
+
+            return paddedData;
+        }
+
+        public static byte[] UnpadIso7816(byte[] data, int blockSize)
+        {
+            if (data == null)
+                throw new ArgumentNullException("data");
+
+            if (data.Length % blockSize != 0 || data.Length == 0)
+                throw new PaddingException("Data is not padded");
+
+            int unpadLength;
+            for (unpadLength = data.Length - 1; unpadLength >= 0; unpadLength--)
+                if (data[unpadLength] == 0x80)
+                    break;
+
+            if (unpadLength == 0)
+                throw new PaddingException("Invalid Iso7816 padding");
+
+            byte[] unpaddedData = new byte[unpadLength];
+            Array.Copy(data, 0, unpaddedData, 0, unpadLength);
+
+            return unpaddedData;
+        }
+
+        public static byte[] PadIso10126(byte[] data, int blockSize)
+        {
+            if (data == null)
+                throw new ArgumentNullException("data");
+
+            int paddingLength = blockSize - data.Length % blockSize;
+
+            byte[] paddedData = new byte[data.Length + paddingLength];
+            Array.Copy(data, 0, paddedData, 0, data.Length);
+            byte[] rndBytes = RandomNumberGenerator.GetBytes(paddingLength - 1);
+            Array.Copy(rndBytes, 0, paddedData, data.Length, paddingLength - 1);
+            paddedData[paddedData.Length - 1] = (byte)paddingLength;
+
+            return paddedData;
+        }
+
+        public static byte[] UnpadIso10126(byte[] data, int blockSize)
+        {
+            if (data == null)
+                throw new ArgumentNullException("data");
+
+            if (data.Length % blockSize != 0 || data.Length == 0)
+                throw new PaddingException("Data is not padded");
+
+            byte paddingLength = data[data.Length - 1];
+
+            byte[] unpaddedData = new byte[data.Length - paddingLength];
+            Array.Copy(data, 0, unpaddedData, 0, data.Length - paddingLength);
+
+            return unpaddedData;
+        }
+    }
+}
---
+++ Byte.Toolkit.Crypto/PubKey/RSA.cs
@@ -0,0 +1,211 @@
+﻿using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Byte.Toolkit.Crypto.PubKey
+{
+    public static class RSA
+    {
+        /// <summary>
+        /// Generate a new RSA key pair
+        /// </summary>
+        /// <param name="keySize">Key size in bits</param>
+        public static RSACryptoServiceProvider GenerateKeyPair(int keySize = 4096)
+        {
+            return new RSACryptoServiceProvider(keySize);
+        }
+
+        #region Encrypt / Decrypt data
+
+        /// <summary>
+        /// Encrypt data using RSACryptoServiceProvider
+        /// </summary>
+        /// <param name="rsa">RSA Key</param>
+        /// <param name="data">Data to encrypt</param>
+        public static byte[] Encrypt(RSACryptoServiceProvider rsa, byte[] data)
+        {
+            return rsa.Encrypt(data, true);
+        }
+
+        /// <summary>
+        /// Decrypt data using RSACryptoServiceProvider
+        /// </summary>
+        /// <param name="rsa">RSA Key</param>
+        /// <param name="encrypted">Data to decrypt</param>
+        public static byte[] Decrypt(RSACryptoServiceProvider rsa, byte[] encrypted)
+        {
+            return rsa.Decrypt(encrypted, true);
+        }
+
+        #endregion
+
+        #region Save / Load PEM files
+
+        /// <summary>
+        ///Load a RSA key from a PEM file 
+        /// </summary>
+        /// <param name="file">PEM file</param>
+        /// <param name="password">Password</param>
+        public static RSACryptoServiceProvider LoadFromPEM(Stream input, string password = null)
+        {
+            using (StreamReader sr = new StreamReader(input, Encoding.Default))
+            {
+                PemReader pemReader;
+                if (!string.IsNullOrEmpty(password))
+                    pemReader = new PemReader(sr, new PasswordFinder(password));
+                else
+                    pemReader = new PemReader(sr);
+
+                RSAParameters parameters;
+                object obj = pemReader.ReadObject();
+
+                if (obj == null)
+                    throw new PemException("PemReader.ReadObject() returned null");
+
+                Type objType = obj.GetType();
+
+                if (objType == typeof(AsymmetricCipherKeyPair))
+                {
+                    AsymmetricCipherKeyPair ackp = (AsymmetricCipherKeyPair)obj;
+                    parameters = DotNetUtilities.ToRSAParameters((RsaPrivateCrtKeyParameters)ackp.Private);
+                }
+                else if (objType == typeof(RsaPrivateCrtKeyParameters))
+                    parameters = DotNetUtilities.ToRSAParameters((RsaPrivateCrtKeyParameters)obj);
+                else if (objType == typeof(RsaKeyParameters))
+                    parameters = DotNetUtilities.ToRSAParameters((RsaKeyParameters)obj);
+                else
+                    throw new PemException($"Cannot handle type '{objType}' returned by PemReader.ReadObject()");
+
+                RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+                rsa.ImportParameters(parameters);
+                return rsa;
+            }
+        }
+
+        /// <summary>
+        /// Save a public RSA key to a PEM file
+        /// </summary>
+        /// <param name="rsa">Public key</param>
+        /// <param name="file">PEM file</param>
+        public static void SavePublicKeyToPEM(RSACryptoServiceProvider rsa, Stream output)
+        {
+            using (StreamWriter sw = new StreamWriter(output, Encoding.Default))
+            {
+                PemWriter pemWriter = new PemWriter(sw);
+                RsaKeyParameters rkp = DotNetUtilities.GetRsaPublicKey(rsa);
+                pemWriter.WriteObject(rkp);
+            }
+        }
+
+        /// <summary>
+        /// Save an encrypted private RSA key to a PEM file
+        /// </summary>
+        /// <param name="rsa">Private key</param>
+        /// <param name="file">PEM file</param>
+        /// <param name="password">Password</param>
+        /// <param name="algorithm">Algorithm for PEM encryption</param>
+        public static void SavePrivateKeyToPEM(RSACryptoServiceProvider rsa, Stream output, string password, string algorithm = "AES-256-CBC")
+        {
+            using (StreamWriter sw = new StreamWriter(output, Encoding.Default))
+            {
+                PemWriter pemWriter = new PemWriter(sw);
+                AsymmetricCipherKeyPair ackp = DotNetUtilities.GetRsaKeyPair(rsa);
+                RsaPrivateCrtKeyParameters privKey = (RsaPrivateCrtKeyParameters)ackp.Private;
+                pemWriter.WriteObject(privKey, algorithm, password.ToCharArray(), new SecureRandom());
+            }
+        }
+
+        /// <summary>
+        /// Save a private RSA key to a PEM file
+        /// </summary>
+        /// <param name="rsa">Private key</param>
+        /// <param name="file">PEM file</param>
+        public static void SavePrivateKeyToPEM(RSACryptoServiceProvider rsa, Stream output)
+        {
+            using (StreamWriter sw = new StreamWriter(output, Encoding.Default))
+            {
+                PemWriter pemWriter = new PemWriter(sw);
+                AsymmetricCipherKeyPair ackp = DotNetUtilities.GetRsaKeyPair(rsa);
+                RsaPrivateCrtKeyParameters privKey = (RsaPrivateCrtKeyParameters)ackp.Private;
+                pemWriter.WriteObject(privKey);
+            }
+        }
+
+        #endregion
+
+        #region Save / Load Win KeyStore
+
+        /// <summary>
+        /// Save a RSA key in the Windows KeyStore
+        /// </summary>
+        /// <param name="rsa">RSA key</param>
+        /// <param name="ContainerName">Container name</param>
+        /// <param name="csppf">CspProviderFlags</param>
+        public static void SaveInWinKeyStore(RSACryptoServiceProvider rsa, string ContainerName, CspProviderFlags csppf = CspProviderFlags.UseMachineKeyStore)
+        {
+            CspParameters cp = new CspParameters();
+            cp.KeyContainerName = ContainerName;
+            cp.Flags = csppf;
+
+            using (RSACryptoServiceProvider winKSkey = new RSACryptoServiceProvider(cp))
+            {
+                winKSkey.FromXmlString(rsa.ToXmlString(true));
+            }
+        }
+
+        /// <summary>
+        /// Load a RSA key from the Windows KeyStore 
+        /// </summary>
+        /// <param name="ContainerName">Container name</param>
+        /// <param name="csppf">CspProviderFlags</param>
+        public static RSACryptoServiceProvider LoadFromWinKeyStore(string ContainerName, CspProviderFlags csppf = CspProviderFlags.UseMachineKeyStore)
+        {
+            CspParameters cp = new CspParameters();
+            cp.KeyContainerName = ContainerName;
+            cp.Flags = csppf;
+
+            return new RSACryptoServiceProvider(cp);
+        }
+
+        /// <summary>
+        /// Delete a RSA key from the Windows KeyStore
+        /// </summary>
+        /// <param name="ContainerName">Container name</param>
+        /// <param name="csppf">CspProviderFlags</param>
+        public static void DeleteFromWinKeyStore(string ContainerName, CspProviderFlags csppf = CspProviderFlags.UseMachineKeyStore)
+        {
+            CspParameters cp = new CspParameters();
+            cp.KeyContainerName = ContainerName;
+            cp.Flags = csppf;
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(cp))
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.Clear();
+            }
+        }
+
+        #endregion
+
+        class PasswordFinder : IPasswordFinder
+        {
+            private string _password;
+
+            public PasswordFinder(string password)
+            {
+                _password = password;
+            }
+
+            public char[] GetPassword()
+            {
+                return _password.ToCharArray();
+            }
+        }
+    }
+}
