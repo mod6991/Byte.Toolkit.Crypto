5bf3f20e505325c25be255cedc88b0c8ea466a32
Josué Clément <josue.clement@petitpierre.ch>
2021-12-16 14:00:50 +0100

    Add PBKDF2 tests

---
+++ CryptoToolkitUnitTests/PBKDF2Tests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Byte.Toolkit.Crypto.KDF;
+using Byte.Toolkit.Crypto.IO;
+using NUnit.Framework;
+
+namespace CryptoToolkitUnitTests
+{
+    public class PBKDF2Tests
+    {
+        [TestCaseSource(nameof(AdvancedTestsSource))]
+        public void AdvancedTests(Tuple<string, string, string> results)
+        {
+            string password = results.Item1;
+            byte[] salt = Hex.Decode(results.Item2);
+            string hexKey = results.Item3;
+
+            byte[] key = PBKDF2.GenerateKeyFromPassword(32, password, salt, 50000);
+
+            Assert.AreEqual(hexKey, Hex.Encode(key));
+        }
+
+        static IEnumerable<Tuple<string, string, string>> AdvancedTestsSource()
+        {
+            using (FileStream fs = new FileStream(@"data/pbkdf2.csv", FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader sr = new StreamReader(fs, Encoding.UTF8))
+                {
+                    sr.ReadLine(); // header
+
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        if (!string.IsNullOrWhiteSpace(line))
+                        {
+                            string[] sp = line.Split(',');
+                            yield return new Tuple<string, string, string>(sp[0], sp[1], sp[2]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CryptoToolkitUnitTests/data/scripts/pbkdf2.py
@@ -0,0 +1,15 @@
+from Crypto.Random import get_random_bytes
+from Crypto.Protocol.KDF import PBKDF2
+from Crypto.Hash import SHA1
+
+def main():
+    with open(r'data\pbkdf2.csv', 'w') as file:
+        file.write('Password,Salt,Key\n')
+        for i in range(1, 100 + 1):
+            password = get_random_bytes(8).hex()
+            salt = get_random_bytes(16)
+            key = PBKDF2(password, salt, 32, count=50000, hmac_hash_module=SHA1).hex()
+            file.write(f"{password},{salt.hex()},{key}\n")
+    
+if __name__ == '__main__':
+    main()
