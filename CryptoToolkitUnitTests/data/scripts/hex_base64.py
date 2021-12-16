e0c6dfb18457dd3f17223b206f3cf42ea1b391c3
Josué Clément <josue.clement@petitpierre.ch>
2021-12-16 09:54:36 +0100

    update hex_base64 script and csv data

--- CryptoToolkitUnitTests/data/scripts/hex_base64.py
+++ CryptoToolkitUnitTests/data/scripts/hex_base64.py
@@ -2,9 +2,9 @@ from Crypto.Random import get_random_bytes
 from base64 import b64encode
 
 def main():
-    with open('hex_base64.csv', 'w') as file:
+    with open(r'data\hex_base64.csv', 'w') as file:
         file.write('Hex,Base64\n')
-        for i in range(1, 1001):
+        for i in range(1, 500 + 1):
             data = get_random_bytes(i)
             file.write(f"{data.hex()},{b64encode(data).decode()}\n")
 
