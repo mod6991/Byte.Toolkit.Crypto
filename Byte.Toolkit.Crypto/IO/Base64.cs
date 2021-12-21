0e6e59c50246d172dd1706d93fca30ec891b8eea
Josue Clement <mod6991@gmail.com>
2021-12-21 20:56:57 +0100

    Improve exceptions

--- Byte.Toolkit.Crypto/IO/Base64.cs
+++ Byte.Toolkit.Crypto/IO/Base64.cs
@@ -78,36 +78,34 @@ namespace Byte.Toolkit.Crypto.IO
                 throw new ArgumentNullException(nameof(str));
 
             if (str.Length % 4 != 0)
-                throw new Base64DecodeException("Invalid input string length (not a multiple of 4)");
+                throw new Base64DecodeException($"Invalid input string length {str.Length}: not a multiple of 4");
 
             byte[] data = new byte[(str.Length / 4) * 3];
             int loops = str.Length / 4;
-            int i;
             int b1, b2, b3, b4;
             int paddings = 0;
 
-            for (i = 0; i < loops; i++)
+            for (int i = 0; i < loops; i++)
             {
                 b1 = CHARS.IndexOf(str[i * 4]);
                 if (b1 == -1)
-                    throw new Base64DecodeException($"Invalid base64 char1 '{str[i * 4]}'");
+                    throw new Base64DecodeException($"Invalid Base64 char '{str[i * 4]}' at position {i * 4}");
 
                 b2 = CHARS.IndexOf(str[i * 4 + 1]);
                 if (b2 == -1)
-                    throw new Base64DecodeException($"Invalid base64 char2 '{str[i * 4 + 1]}'");
+                    throw new Base64DecodeException($"Invalid Base64 char '{str[i * 4 + 1]}' at position {i * 4 + 1}");
 
                 b3 = CHARS.IndexOf(str[i * 4 + 2]);
                 if (b3 == -1 && str[i * 4 + 2] != PADDING)
-                    throw new Base64DecodeException($"Invalid base64 char3 '{str[i * 4 + 2]}'");
+                    throw new Base64DecodeException($"Invalid Base64 char '{str[i * 4 + 2]}' at position  {i * 4 + 2}");
                 if (str[i * 4 + 2] == PADDING)
                     paddings++;
 
                 b4 = CHARS.IndexOf(str[i * 4 + 3]);
                 if (b4 == -1 && str[i * 4 + 3] != PADDING)
-                    throw new Base64DecodeException($"Invalid base64 char4 '{str[i * 4 + 3]}'");
+                    throw new Base64DecodeException($"Invalid Base64 char '{str[i * 4 + 3]}' at position  {i * 4 + 3}");
                 if (str[i * 4 + 3] == PADDING)
                     paddings++;
-
 
                 data[i * 3] = (byte)(b1 << 2 | b2 >> 4);
                 data[i * 3 + 1] = (byte)((b2 & 0x0f) << 4 | b3 >> 2);
