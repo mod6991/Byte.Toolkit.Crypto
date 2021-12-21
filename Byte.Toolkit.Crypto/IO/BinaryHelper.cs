62d2d02466d5175a6beaa85e32e9383fcd2b731f
Josue Clement <mod6991@gmail.com>
2021-12-21 21:20:49 +0100

    Improve exceptions

--- Byte.Toolkit.Crypto/IO/BinaryHelper.cs
+++ Byte.Toolkit.Crypto/IO/BinaryHelper.cs
@@ -11,8 +11,12 @@ namespace Byte.Toolkit.Crypto.IO
         /// </summary>
         /// <param name="stream">Output stream</param>
         /// <param name="value">Byte value</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static void Write(Stream stream, byte value)
         {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
             stream.Write(new byte[] { value }, 0, 1);
         }
 
@@ -21,8 +25,14 @@ namespace Byte.Toolkit.Crypto.IO
         /// </summary>
         /// <param name="stream">Output stream</param>
         /// <param name="value">Bytes value</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static void Write(Stream stream, byte[] value)
         {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+
             stream.Write(value, 0, value.Length);
         }
 
@@ -31,8 +41,12 @@ namespace Byte.Toolkit.Crypto.IO
         /// </summary>
         /// <param name="stream">Output stream</param>
         /// <param name="value">Bool value</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static void Write(Stream stream, bool value)
         {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
             byte[] data = BitConverter.GetBytes(value);
             stream.Write(data, 0, 1);
         }
@@ -42,8 +56,12 @@ namespace Byte.Toolkit.Crypto.IO
         /// </summary>
         /// <param name="stream">Output stream</param>
         /// <param name="value">Int16 value</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static void Write(Stream stream, Int16 value)
         {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
             byte[] data = BitConverter.GetBytes(value);
             stream.Write(data, 0, 2);
         }
@@ -53,8 +71,12 @@ namespace Byte.Toolkit.Crypto.IO
         /// </summary>
         /// <param name="stream">Output stream</param>
         /// <param name="value">UInt16 value</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static void Write(Stream stream, UInt16 value)
         {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
             byte[] data = BitConverter.GetBytes(value);
             stream.Write(data, 0, 2);
         }
@@ -64,8 +86,12 @@ namespace Byte.Toolkit.Crypto.IO
         /// </summary>
         /// <param name="stream">Output stream</param>
         /// <param name="value">Int32 value</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static void Write(Stream stream, Int32 value)
         {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
             byte[] data = BitConverter.GetBytes(value);
             stream.Write(data, 0, 4);
         }
@@ -75,8 +101,12 @@ namespace Byte.Toolkit.Crypto.IO
         /// </summary>
         /// <param name="stream">Output stream</param>
         /// <param name="value">UInt32 value</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static void Write(Stream stream, UInt32 value)
         {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
             byte[] data = BitConverter.GetBytes(value);
             stream.Write(data, 0, 4);
         }
@@ -86,8 +116,12 @@ namespace Byte.Toolkit.Crypto.IO
         /// </summary>
         /// <param name="stream">Output stream</param>
         /// <param name="value">Int64 value</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static void Write(Stream stream, Int64 value)
         {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
             byte[] data = BitConverter.GetBytes(value);
             stream.Write(data, 0, 8);
         }
@@ -97,8 +131,12 @@ namespace Byte.Toolkit.Crypto.IO
         /// </summary>
         /// <param name="stream">Output stream</param>
         /// <param name="value">UInt64 value</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static void Write(Stream stream, UInt64 value)
         {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
             byte[] data = BitConverter.GetBytes(value);
             stream.Write(data, 0, 8);
         }
@@ -108,8 +146,12 @@ namespace Byte.Toolkit.Crypto.IO
         /// </summary>
         /// <param name="stream">Output stream</param>
         /// <param name="value">float value</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static void Write(Stream stream, float value)
         {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
             byte[] data = BitConverter.GetBytes(value);
             stream.Write(data, 0, 4);
         }
@@ -119,8 +161,12 @@ namespace Byte.Toolkit.Crypto.IO
         /// </summary>
         /// <param name="stream">Output stream</param>
         /// <param name="value">double value</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static void Write(Stream stream, double value)
         {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
             byte[] data = BitConverter.GetBytes(value);
             stream.Write(data, 0, 8);
         }
@@ -131,8 +177,16 @@ namespace Byte.Toolkit.Crypto.IO
         /// <param name="stream">Output stream</param>
         /// <param name="value">String value</param>
         /// <param name="encoding">String encoding</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static void Write(Stream stream, string value, Encoding encoding)
         {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+            if (encoding == null)
+                throw new ArgumentNullException(nameof(encoding));
+
             byte[] data = encoding.GetBytes(value);
             stream.Write(data, 0, data.Length);
         }
@@ -142,8 +196,14 @@ namespace Byte.Toolkit.Crypto.IO
         /// </summary>
         /// <param name="stream">Output stream</param>
         /// <param name="value">Value</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static void WriteLV(Stream stream, byte[] value)
         {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+
             Write(stream, value.Length);
             Write(stream, value);
         }
@@ -152,8 +212,12 @@ namespace Byte.Toolkit.Crypto.IO
         /// Read byte from stream
         /// </summary>
         /// <param name="stream">Input stream</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static byte ReadByte(Stream stream)
         {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
             byte[] buffer = new byte[1];
 
             if (stream.Read(buffer, 0, 1) != 1)
@@ -167,8 +231,12 @@ namespace Byte.Toolkit.Crypto.IO
         /// </summary>
         /// <param name="stream">Input stream</param>
         /// <param name="nbBytes">Number of bytes to read</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static byte[] ReadBytes(Stream stream, int nbBytes)
         {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
             byte[] buffer = new byte[nbBytes];
 
             if (stream.Read(buffer, 0, nbBytes) != nbBytes)
@@ -181,8 +249,12 @@ namespace Byte.Toolkit.Crypto.IO
         /// Read bool from stream
         /// </summary>
         /// <param name="stream">Input stream</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static bool ReadBool(Stream stream)
         {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
             byte[] buffer = new byte[1];
 
             if (stream.Read(buffer, 0, 1) != 1)
@@ -195,8 +267,12 @@ namespace Byte.Toolkit.Crypto.IO
         /// Read Int16 from stream
         /// </summary>
         /// <param name="stream">Input stream</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static Int16 ReadInt16(Stream stream)
         {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
             byte[] buffer = new byte[2];
 
             if (stream.Read(buffer, 0, 2) != 2)
@@ -209,8 +285,12 @@ namespace Byte.Toolkit.Crypto.IO
         /// Read UInt16 from stream
         /// </summary>
         /// <param name="stream">Input stream</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static UInt16 ReadUInt16(Stream stream)
         {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
             byte[] buffer = new byte[2];
 
             if (stream.Read(buffer, 0, 2) != 2)
@@ -223,8 +303,12 @@ namespace Byte.Toolkit.Crypto.IO
         /// Read Int32 from stream
         /// </summary>
         /// <param name="stream">Input stream</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static Int32 ReadInt32(Stream stream)
         {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
             byte[] buffer = new byte[4];
 
             if (stream.Read(buffer, 0, 4) != 4)
@@ -237,8 +321,12 @@ namespace Byte.Toolkit.Crypto.IO
         /// Read UInt32 from stream
         /// </summary>
         /// <param name="stream">Input stream</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static UInt32 ReadUInt32(Stream stream)
         {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
             byte[] buffer = new byte[4];
 
             if (stream.Read(buffer, 0, 4) != 4)
@@ -251,8 +339,12 @@ namespace Byte.Toolkit.Crypto.IO
         /// Read Int64 from stream
         /// </summary>
         /// <param name="stream">Input stream</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static Int64 ReadInt64(Stream stream)
         {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
             byte[] buffer = new byte[8];
 
             if (stream.Read(buffer, 0, 8) != 8)
@@ -265,8 +357,12 @@ namespace Byte.Toolkit.Crypto.IO
         /// Read UInt64 from stream
         /// </summary>
         /// <param name="stream">Input stream</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static UInt64 ReadUInt64(Stream stream)
         {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
             byte[] buffer = new byte[8];
 
             if (stream.Read(buffer, 0, 8) != 8)
@@ -279,8 +375,12 @@ namespace Byte.Toolkit.Crypto.IO
         /// Read float from stream
         /// </summary>
         /// <param name="stream">Input stream</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static float ReadFloat(Stream stream)
         {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
             byte[] buffer = new byte[4];
 
             if (stream.Read(buffer, 0, 4) != 4)
@@ -293,8 +393,12 @@ namespace Byte.Toolkit.Crypto.IO
         /// Read double from stream
         /// </summary>
         /// <param name="stream">Input stream</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static double ReadDouble(Stream stream)
         {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
             byte[] buffer = new byte[8];
 
             if (stream.Read(buffer, 0, 8) != 8)
@@ -307,8 +411,12 @@ namespace Byte.Toolkit.Crypto.IO
         /// Read a Length-Value
         /// </summary>
         /// <param name="stream">Input stream</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static byte[] ReadLV(Stream stream)
         {
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
             int valueLength = ReadInt32(stream);
             return ReadBytes(stream, valueLength);
         }
