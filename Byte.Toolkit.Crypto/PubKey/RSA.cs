705a99d6651946108d26df73bbb5c1cbd84a2ad0
Josue Clement <mod6991@gmail.com>
2021-12-22 18:35:04 +0100

    Improve exceptions and comments

--- Byte.Toolkit.Crypto/PubKey/RSA.cs
+++ Byte.Toolkit.Crypto/PubKey/RSA.cs
@@ -9,6 +9,9 @@ using System.Text;
 
 namespace Byte.Toolkit.Crypto.PubKey
 {
+    /// <summary>
+    /// RSA helper class for key generation, data encrypt/decrypt and key load/save
+    /// </summary>
     public static class RSA
     {
         /// <summary>
@@ -27,8 +30,14 @@ namespace Byte.Toolkit.Crypto.PubKey
         /// </summary>
         /// <param name="rsa">RSA Key</param>
         /// <param name="data">Data to encrypt</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static byte[] Encrypt(RSACryptoServiceProvider rsa, byte[] data)
         {
+            if (rsa == null)
+                throw new ArgumentNullException(nameof(rsa));
+            if (data == null)
+                throw new ArgumentNullException(nameof(data));
+
             return rsa.Encrypt(data, true);
         }
 
@@ -37,8 +46,14 @@ namespace Byte.Toolkit.Crypto.PubKey
         /// </summary>
         /// <param name="rsa">RSA Key</param>
         /// <param name="encrypted">Data to decrypt</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static byte[] Decrypt(RSACryptoServiceProvider rsa, byte[] encrypted)
         {
+            if (rsa == null)
+                throw new ArgumentNullException(nameof(rsa));
+            if (encrypted == null)
+                throw new ArgumentNullException(nameof(encrypted));
+
             return rsa.Decrypt(encrypted, true);
         }
 
@@ -51,8 +66,12 @@ namespace Byte.Toolkit.Crypto.PubKey
         /// </summary>
         /// <param name="input">Input stream</param>
         /// <param name="password">Password</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static RSACryptoServiceProvider LoadFromPEM(Stream input, string password = null)
         {
+            if (input == null)
+                throw new ArgumentNullException(nameof(input));
+
             using (StreamReader sr = new StreamReader(input, Encoding.Default))
             {
                 PemReader pemReader;
@@ -92,8 +111,12 @@ namespace Byte.Toolkit.Crypto.PubKey
         /// </summary>
         /// <param name="filePath">PEM file path</param>
         /// <param name="password">Password</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static RSACryptoServiceProvider LoadFromPEM(string filePath, string password = null)
         {
+            if (filePath == null)
+                throw new ArgumentNullException(nameof(filePath));
+
             using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
             {
                 return LoadFromPEM(fs, password);
@@ -105,8 +128,14 @@ namespace Byte.Toolkit.Crypto.PubKey
         /// </summary>
         /// <param name="rsa">Public key</param>
         /// <param name="output">Output stream</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static void SavePublicKeyToPEM(RSACryptoServiceProvider rsa, Stream output)
         {
+            if (rsa == null)
+                throw new ArgumentNullException(nameof(rsa));
+            if (output == null)
+                throw new ArgumentNullException(nameof(output));
+
             using (StreamWriter sw = new StreamWriter(output, Encoding.Default))
             {
                 PemWriter pemWriter = new PemWriter(sw);
@@ -120,8 +149,14 @@ namespace Byte.Toolkit.Crypto.PubKey
         /// </summary>
         /// <param name="rsa">Public key</param>
         /// <param name="outputFile">PEM output file</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static void SavePublicKeyToPEM(RSACryptoServiceProvider rsa, string outputFile)
         {
+            if (rsa == null)
+                throw new ArgumentNullException(nameof(rsa));
+            if (outputFile == null)
+                throw new ArgumentNullException(nameof(outputFile));
+
             using (FileStream fs = new FileStream(outputFile, FileMode.Create, FileAccess.Write))
             {
                 SavePublicKeyToPEM(rsa, fs);
@@ -135,8 +170,18 @@ namespace Byte.Toolkit.Crypto.PubKey
         /// <param name="output">Output stream</param>
         /// <param name="password">Password</param>
         /// <param name="algorithm">Algorithm for PEM encryption</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static void SavePrivateKeyToPEM(RSACryptoServiceProvider rsa, Stream output, string password, string algorithm = "AES-256-CBC")
         {
+            if (rsa == null)
+                throw new ArgumentNullException(nameof(rsa));
+            if (output == null)
+                throw new ArgumentNullException(nameof(output));
+            if (password == null)
+                throw new ArgumentNullException(nameof(password));
+            if (algorithm == null)
+                throw new ArgumentNullException(nameof(algorithm));
+
             using (StreamWriter sw = new StreamWriter(output, Encoding.Default))
             {
                 PemWriter pemWriter = new PemWriter(sw);
@@ -153,8 +198,18 @@ namespace Byte.Toolkit.Crypto.PubKey
         /// <param name="outputFile">PEM output file</param>
         /// <param name="password">Password</param>
         /// <param name="algorithm">Algorithm</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static void SavePrivateKeyToPEM(RSACryptoServiceProvider rsa, string outputFile, string password, string algorithm = "AES-256-CBC")
         {
+            if (rsa == null)
+                throw new ArgumentNullException(nameof(rsa));
+            if (outputFile == null)
+                throw new ArgumentNullException(nameof(outputFile));
+            if (password == null)
+                throw new ArgumentNullException(nameof(password));
+            if (algorithm == null)
+                throw new ArgumentNullException(nameof(algorithm));
+
             using (FileStream fs = new FileStream(outputFile, FileMode.Create, FileAccess.Write))
             {
                 SavePrivateKeyToPEM(rsa, fs, password, algorithm);
@@ -166,8 +221,14 @@ namespace Byte.Toolkit.Crypto.PubKey
         /// </summary>
         /// <param name="rsa">Private key</param>
         /// <param name="output">Output stream</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static void SavePrivateKeyToPEM(RSACryptoServiceProvider rsa, Stream output)
         {
+            if (rsa == null)
+                throw new ArgumentNullException(nameof(rsa));
+            if (output == null)
+                throw new ArgumentNullException(nameof(output));
+
             using (StreamWriter sw = new StreamWriter(output, Encoding.Default))
             {
                 PemWriter pemWriter = new PemWriter(sw);
@@ -177,8 +238,19 @@ namespace Byte.Toolkit.Crypto.PubKey
             }
         }
 
+        /// <summary>
+        /// Save a private RSA key to a PEM file
+        /// </summary>
+        /// <param name="rsa"></param>
+        /// <param name="outputFile"></param>
+        /// <exception cref="ArgumentNullException"></exception>
         public static void SavePrivateKeyToPEM(RSACryptoServiceProvider rsa, string outputFile)
         {
+            if (rsa == null)
+                throw new ArgumentNullException(nameof(rsa));
+            if (outputFile == null)
+                throw new ArgumentNullException(nameof(outputFile));
+
             using (FileStream fs = new FileStream(outputFile, FileMode.Create, FileAccess.Write))
             {
                 SavePrivateKeyToPEM(rsa, fs);
@@ -193,12 +265,18 @@ namespace Byte.Toolkit.Crypto.PubKey
         /// Save a RSA key in the Windows KeyStore
         /// </summary>
         /// <param name="rsa">RSA key</param>
-        /// <param name="ContainerName">Container name</param>
+        /// <param name="containerName">Container name</param>
         /// <param name="csppf">CspProviderFlags</param>
-        public static void SaveInWinKeyStore(RSACryptoServiceProvider rsa, string ContainerName, CspProviderFlags csppf = CspProviderFlags.UseMachineKeyStore)
+        /// <exception cref="ArgumentNullException"></exception>
+        public static void SaveInWinKeyStore(RSACryptoServiceProvider rsa, string containerName, CspProviderFlags csppf = CspProviderFlags.UseMachineKeyStore)
         {
+            if (rsa == null)
+                throw new ArgumentNullException(nameof(rsa));
+            if (containerName == null)
+                throw new ArgumentNullException(nameof(containerName));
+
             CspParameters cp = new CspParameters();
-            cp.KeyContainerName = ContainerName;
+            cp.KeyContainerName = containerName;
             cp.Flags = csppf;
 
             using (RSACryptoServiceProvider winKSkey = new RSACryptoServiceProvider(cp))
@@ -210,12 +288,16 @@ namespace Byte.Toolkit.Crypto.PubKey
         /// <summary>
         /// Load a RSA key from the Windows KeyStore 
         /// </summary>
-        /// <param name="ContainerName">Container name</param>
+        /// <param name="containerName">Container name</param>
         /// <param name="csppf">CspProviderFlags</param>
-        public static RSACryptoServiceProvider LoadFromWinKeyStore(string ContainerName, CspProviderFlags csppf = CspProviderFlags.UseMachineKeyStore)
+        /// <exception cref="ArgumentNullException"></exception>
+        public static RSACryptoServiceProvider LoadFromWinKeyStore(string containerName, CspProviderFlags csppf = CspProviderFlags.UseMachineKeyStore)
         {
+            if (containerName == null)
+                throw new ArgumentNullException(nameof(containerName));
+
             CspParameters cp = new CspParameters();
-            cp.KeyContainerName = ContainerName;
+            cp.KeyContainerName = containerName;
             cp.Flags = csppf;
 
             return new RSACryptoServiceProvider(cp);
@@ -224,12 +306,16 @@ namespace Byte.Toolkit.Crypto.PubKey
         /// <summary>
         /// Delete a RSA key from the Windows KeyStore
         /// </summary>
-        /// <param name="ContainerName">Container name</param>
+        /// <param name="containerName">Container name</param>
         /// <param name="csppf">CspProviderFlags</param>
-        public static void DeleteFromWinKeyStore(string ContainerName, CspProviderFlags csppf = CspProviderFlags.UseMachineKeyStore)
+        /// <exception cref="ArgumentNullException"></exception>
+        public static void DeleteFromWinKeyStore(string containerName, CspProviderFlags csppf = CspProviderFlags.UseMachineKeyStore)
         {
+            if (containerName == null)
+                throw new ArgumentNullException(nameof(containerName));
+
             CspParameters cp = new CspParameters();
-            cp.KeyContainerName = ContainerName;
+            cp.KeyContainerName = containerName;
             cp.Flags = csppf;
             using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(cp))
             {
@@ -240,6 +326,9 @@ namespace Byte.Toolkit.Crypto.PubKey
 
         #endregion
 
+        /// <summary>
+        /// Password finder helper used by PemReader
+        /// </summary>
         class PasswordFinder : IPasswordFinder
         {
             private string _password;
@@ -249,6 +338,10 @@ namespace Byte.Toolkit.Crypto.PubKey
                 _password = password;
             }
 
+            /// <summary>
+            /// Get password stored
+            /// </summary>
+            /// <returns>Password</returns>
             public char[] GetPassword()
             {
                 return _password.ToCharArray();
