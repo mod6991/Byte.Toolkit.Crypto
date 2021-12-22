bb64a53b9bafdc64b53a5ccb381e5128e82f2088
Josue Clement <mod6991@gmail.com>
2021-12-22 18:15:28 +0100

    Finish AesFileEnc

--- Byte.Toolkit.Crypto/FileEnc/AesFileEnc.cs
+++ Byte.Toolkit.Crypto/FileEnc/AesFileEnc.cs
@@ -11,20 +11,24 @@ using System.Text;
 namespace Byte.Toolkit.Crypto.FileEnc
 {
     /// <summary>
-    /// Encrypt/Decrypt files with AES-256-CBC with RSA key or password
+    /// Encrypt/Decrypt files with AES-256 with RSA key or password
     /// </summary>
     public static class AesFileEnc
     {
-        private const byte _version = 0x04;
+        private const byte VERSION = 0x05;
+        private const int BUFFER_SIZE = 4096;
+        private const string RSA_HEADER = "AENCR!";
+        private const string PASS_HEADER = "AENCP!";
+        private const int SALT_SIZE = 16;
 
         /// <summary>
-        /// 
+        /// Encrypt with AES-256 with a RSA key
         /// </summary>
-        /// <param name="input"></param>
-        /// <param name="output"></param>
-        /// <param name="rsa"></param>
-        /// <param name="keyName"></param>
-        /// <param name="notifyProgression"></param>
+        /// <param name="input">Input stream</param>
+        /// <param name="output">Output stream</param>
+        /// <param name="rsa">RSA key</param>
+        /// <param name="keyName">Key name</param>
+        /// <param name="notifyProgression">Notify progression delegate</param>
         /// <exception cref="ArgumentNullException"></exception>
         public static void Encrypt(Stream input, Stream output, RSACryptoServiceProvider rsa, string keyName, Action<int> notifyProgression = null)
         {
@@ -37,17 +41,35 @@ namespace Byte.Toolkit.Crypto.FileEnc
             if (keyName == null)
                 throw new ArgumentNullException(nameof(keyName));
 
-            //TODO!
+            byte[] key = RandomHelper.GenerateBytes(AES.KEY_SIZE);
+            byte[] iv = RandomHelper.GenerateBytes(AES.IV_SIZE);
+
+            byte[] keyData;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                BinaryHelper.WriteLV(ms, key);
+                BinaryHelper.WriteLV(ms, iv);
+                keyData = ms.ToArray();
+            }
+
+            byte[] encKeyData = PubKey.RSA.Encrypt(rsa, keyData);
+
+            BinaryHelper.Write(output, RSA_HEADER, Encoding.ASCII);
+            BinaryHelper.Write(output, VERSION);
+            BinaryHelper.WriteLV(output, Encoding.ASCII.GetBytes(keyName));
+            BinaryHelper.WriteLV(output, encKeyData);
+
+            AES.EncryptCBC(input, output, key, iv, new Pkcs7Padding(), notifyProgression, BUFFER_SIZE);
         }
 
         /// <summary>
-        /// 
+        /// Encrypt file with AES-256 with a RSA key
         /// </summary>
-        /// <param name="inputFile"></param>
-        /// <param name="outputFile"></param>
-        /// <param name="rsa"></param>
-        /// <param name="keyName"></param>
-        /// <param name="notifyProgression"></param>
+        /// <param name="inputFile">Input file</param>
+        /// <param name="outputFile">Output file</param>
+        /// <param name="rsa">RSA key</param>
+        /// <param name="keyName">Key name</param>
+        /// <param name="notifyProgression">Notify progression delegate</param>
         /// <exception cref="ArgumentNullException"></exception>
         public static void Encrypt(string inputFile, string outputFile, RSACryptoServiceProvider rsa, string keyName, Action<int> notifyProgression = null)
         {
@@ -70,12 +92,12 @@ namespace Byte.Toolkit.Crypto.FileEnc
         }
 
         /// <summary>
-        /// 
+        /// Encrypt with AES-256 with a password
         /// </summary>
-        /// <param name="input"></param>
-        /// <param name="output"></param>
-        /// <param name="password"></param>
-        /// <param name="notifyProgression"></param>
+        /// <param name="input">Input file</param>
+        /// <param name="output">Output file</param>
+        /// <param name="password">Password</param>
+        /// <param name="notifyProgression">Notify progression delegate</param>
         /// <exception cref="ArgumentNullException"></exception>
         public static void Encrypt(Stream input, Stream output, string password, Action<int> notifyProgression = null)
         {
@@ -86,29 +108,25 @@ namespace Byte.Toolkit.Crypto.FileEnc
             if (password == null)
                 throw new ArgumentNullException(nameof(password));
 
-            IDataPadding padding = new Pkcs7Padding();
-
-            byte[] salt = RandomHelper.GenerateBytes(16);
+            byte[] salt = RandomHelper.GenerateBytes(SALT_SIZE);
             byte[] key = PBKDF2.GenerateKeyFromPassword(AES.KEY_SIZE, password, salt);
             byte[] iv = RandomHelper.GenerateBytes(AES.IV_SIZE);
 
-            BinaryHelper.Write(output, "ENCP!", Encoding.ASCII);
-            BinaryHelper.Write(output, _version);
-            BinaryHelper.Write(output, (byte)iv.Length);
-            BinaryHelper.Write(output, (byte)salt.Length);
-            BinaryHelper.Write(output, iv);
-            BinaryHelper.Write(output, salt);
+            BinaryHelper.Write(output, PASS_HEADER, Encoding.ASCII);
+            BinaryHelper.Write(output, VERSION);
+            BinaryHelper.WriteLV(output, salt);
+            BinaryHelper.WriteLV(output, iv);
 
-            AES.EncryptCBC(input, output, key, iv, padding, notifyProgression);
+            AES.EncryptCBC(input, output, key, iv, new Pkcs7Padding(), notifyProgression, BUFFER_SIZE);
         }
 
         /// <summary>
-        /// 
+        /// Encrypt file with AES-256 with a password
         /// </summary>
-        /// <param name="inputFile"></param>
-        /// <param name="outputFile"></param>
-        /// <param name="password"></param>
-        /// <param name="notifyProgression"></param>
+        /// <param name="inputFile">Input file</param>
+        /// <param name="outputFile">Output file</param>
+        /// <param name="password">Password</param>
+        /// <param name="notifyProgression">Notify progression delegate</param>
         /// <exception cref="ArgumentNullException"></exception>
         public static void Encrypt(string inputFile, string outputFile, string password, Action<int> notifyProgression = null)
         {
@@ -129,7 +147,7 @@ namespace Byte.Toolkit.Crypto.FileEnc
         }
 
         /// <summary>
-        /// 
+        /// Decrypt with AES-256 with a RSA key
         /// </summary>
         /// <param name="input"></param>
         /// <param name="output"></param>
@@ -145,16 +163,34 @@ namespace Byte.Toolkit.Crypto.FileEnc
             if (rsa == null)
                 throw new ArgumentNullException(nameof(rsa));
 
-            //TODO!
+            input.Seek(RSA_HEADER.Length, SeekOrigin.Current); // Header
+            input.Seek(1, SeekOrigin.Current); // Version
+
+            byte[] keyNameData = BinaryHelper.ReadLV(input);
+            byte[] encKeyData = BinaryHelper.ReadLV(input);
+
+            if (notifyProgression != null)
+                notifyProgression(RSA_HEADER.Length + 1 + 2 * sizeof(int) + keyNameData.Length + encKeyData.Length);
+
+            byte[] keyData = PubKey.RSA.Decrypt(rsa, encKeyData);
+
+            byte[] key, iv;
+            using (MemoryStream ms = new MemoryStream(keyData))
+            {
+                key = BinaryHelper.ReadLV(ms);
+                iv = BinaryHelper.ReadLV(ms);
+            }
+
+            AES.DecryptCBC(input, output, key, iv, new Pkcs7Padding(), notifyProgression, BUFFER_SIZE);
         }
 
         /// <summary>
-        /// 
+        /// Decrypt file with AES-256 with a RSA key
         /// </summary>
-        /// <param name="inputFile"></param>
-        /// <param name="outputFile"></param>
-        /// <param name="rsa"></param>
-        /// <param name="notifyProgression"></param>
+        /// <param name="inputFile">Input file</param>
+        /// <param name="outputFile">Output file</param>
+        /// <param name="rsa">RSA key</param>
+        /// <param name="notifyProgression">Notify progression delegate</param>
         /// <exception cref="ArgumentNullException"></exception>
         public static void Decrypt(string inputFile, string outputFile, RSACryptoServiceProvider rsa, Action<int> notifyProgression = null)
         {
@@ -175,12 +211,12 @@ namespace Byte.Toolkit.Crypto.FileEnc
         }
 
         /// <summary>
-        /// 
+        /// Decrypt with AES-256 with a password
         /// </summary>
-        /// <param name="input"></param>
-        /// <param name="output"></param>
-        /// <param name="password"></param>
-        /// <param name="notifyProgression"></param>
+        /// <param name="input">Input stream</param>
+        /// <param name="output">Output stream</param>
+        /// <param name="password">Password</param>
+        /// <param name="notifyProgression">Notify progression delegate</param>
         /// <exception cref="ArgumentNullException"></exception>
         public static void Decrypt(Stream input, Stream output, string password, Action<int> notifyProgression = null)
         {
@@ -191,31 +227,27 @@ namespace Byte.Toolkit.Crypto.FileEnc
             if (password == null)
                 throw new ArgumentNullException(nameof(password));
 
-            IDataPadding padding = new Pkcs7Padding();
-
-            input.Seek(5, SeekOrigin.Current); // Header
+            input.Seek(PASS_HEADER.Length, SeekOrigin.Current); // Header
             input.Seek(1, SeekOrigin.Current); // Version
 
-            byte ivLength = BinaryHelper.ReadByte(input);
-            byte saltLength = BinaryHelper.ReadByte(input);
-            byte[] iv = BinaryHelper.ReadBytes(input, ivLength);
-            byte[] salt = BinaryHelper.ReadBytes(input, saltLength);
+            byte[] salt = BinaryHelper.ReadLV(input);
+            byte[] iv = BinaryHelper.ReadLV(input);
 
             if (notifyProgression != null)
-                notifyProgression(5 + 1 + 1 + 1 + ivLength + saltLength);
+                notifyProgression(PASS_HEADER.Length + 1 + 2 * sizeof(int) + salt.Length + iv.Length);
 
             byte[] key = PBKDF2.GenerateKeyFromPassword(AES.KEY_SIZE, password, salt);
 
-            AES.DecryptCBC(input, output, key, iv, padding, notifyProgression);
+            AES.DecryptCBC(input, output, key, iv, new Pkcs7Padding(), notifyProgression);
         }
 
         /// <summary>
-        /// 
+        /// Decrypt file with AES-256 with a password
         /// </summary>
-        /// <param name="inputFile"></param>
-        /// <param name="outputFile"></param>
-        /// <param name="password"></param>
-        /// <param name="notifyProgression"></param>
+        /// <param name="inputFile">Input file</param>
+        /// <param name="outputFile">Output file</param>
+        /// <param name="password">Password</param>
+        /// <param name="notifyProgression">Notify progression delegate</param>
         /// <exception cref="ArgumentNullException"></exception>
         public static void Decrypt(string inputFile, string outputFile, string password, Action<int> notifyProgression = null)
         {
