8197452edb5441b85caa2082d45aec80543e26e1
Josue Clement <mod6991@gmail.com>
2021-12-27 14:44:07 +0100

    Add AnsiX923 padding tests

---
+++ CryptoToolkitUnitTests/Padding/AnsiX923Tests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Byte.Toolkit.Crypto.IO;
+using Byte.Toolkit.Crypto.Padding;
+using NUnit.Framework;
+
+namespace CryptoToolkitUnitTests.Padding
+{
+    public class AnsiX923Tests
+    {
+        [TestCaseSource(nameof(DataSource))]
+        public void Pad(Tuple<byte[], byte[]> values)
+        {
+            byte[] padded = new AnsiX923Padding().Pad(values.Item1, 16);
+            Assert.AreEqual(values.Item2, padded);
+        }
+
+        [TestCaseSource(nameof(DataSource))]
+        public void Unpad(Tuple<byte[], byte[]> values)
+        {
+            byte[] unpadded = new AnsiX923Padding().Unpad(values.Item2, 16);
+            Assert.AreEqual(values.Item1, unpadded);
+        }
+
+        [Test]
+        public void PadBadBlockSize()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                new AnsiX923Padding().Pad(new byte[] { }, 0);
+            });
+        }
+
+        [Test]
+        public void PadNull()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                new AnsiX923Padding().Pad(null, 16);
+            });
+        }
+
+        [Test]
+        public void UnpadBadBlockSize()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                new AnsiX923Padding().Unpad(new byte[] { }, 0);
+            });
+        }
+
+        [Test]
+        public void UnPadBadPaddingLength()
+        {
+            Assert.Throws<PaddingException>(() =>
+            {
+                new AnsiX923Padding().Unpad(Hex.Decode("000000000000000000000000000000"), 16);
+            });
+        }
+
+        [Test]
+        public void UnPadNull()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                new AnsiX923Padding().Unpad(null, 16);
+            });
+        }
+
+        static IEnumerable<Tuple<byte[], byte[]>> DataSource()
+        {
+            using (FileStream fsDat = StreamHelper.GetFileStreamOpen(@"data\Padding\ansix923_data.dat"))
+            {
+                using (FileStream fsPadded = StreamHelper.GetFileStreamOpen(@"data\Padding\ansix923_padded.dat"))
+                {
+                    int total = BinaryHelper.ReadInt32(fsDat);
+                    BinaryHelper.ReadInt32(fsPadded);
+
+                    for (int i = 0; i < total; i++)
+                    {
+                        byte[] data = BinaryHelper.ReadLV(fsDat);
+                        byte[] padded = BinaryHelper.ReadLV(fsPadded);
+
+                        yield return new Tuple<byte[], byte[]>(data, padded);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CryptoToolkitUnitTests/data/Padding/ansix923.py
@@ -0,0 +1,39 @@
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
+from struct import pack, unpack
+from typing import BinaryIO
+
+
+def read_lv(stream: BinaryIO):
+    data_len = stream.read(4)
+    value_len = unpack('i', data_len)[0]
+    return stream.read(value_len)
+
+
+def write_lv(stream: BinaryIO, value: bytes):
+    data_len = pack('i', len(value))
+    stream.write(data_len)
+    stream.write(value)
+
+
+def write_l(stream: BinaryIO, value: int):
+    data_len = pack('i', value)
+    stream.write(data_len)
+
+
+def main():
+    with open('ansix923_data.dat', 'wb') as ansix923_data_dat,\
+         open('ansix923_padded.dat', 'wb') as ansix923_padded_dat:
+
+        write_l(ansix923_data_dat, 100)
+        write_l(ansix923_padded_dat, 100)
+
+        for i in range(100):
+            data = get_random_bytes(i)
+            write_lv(ansix923_data_dat, data)
+            padded = pad(data, 16, 'x923')
+            write_lv(ansix923_padded_dat, padded)
+
+
+if __name__ == '__main__':
+    main()
